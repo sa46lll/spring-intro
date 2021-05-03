70eb8f5e017eba3d1d71524ef200f5e7ec320458
sa46lll <9812smh@naver.com>
2021-05-04 01:03:11 +0900

    Jdbc

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 //직접 빈을 스프링에 등록하기
 @Configuration
 public class SpringConfig {
+
+    public DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean //MemberService 빈에 등록
     public MemberService memberService(){
@@ -17,8 +27,8 @@ public class SpringConfig {
 
     @Bean //MemberRepository 빈에 등록
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
