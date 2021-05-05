a406173c114a17d938bc1cb3331075558ca63df5
sa46lll <9812smh@naver.com>
2021-05-05 20:43:03 +0900

    currentTimeMs

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -24,11 +24,21 @@ public class MemberService {
      * 회원 가입
      */
 
-    public Long join(Member member){
+    public Long join(Member member) {
 
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally{ //예외가 터져도 항상 들어옴.
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+
+
 
           // OPTION 2)
 //        Optional<Member> result = memberRepository.findByName(member.getName());
@@ -52,7 +62,14 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId){
